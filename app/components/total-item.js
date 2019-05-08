8f6675e98c1bf6c01490c6f99272e3e3e08f03f0
JamesTArnold <jamestarnold85@gmail.com>
2019-05-08 13:31:49 -0500

    added strike through functionality

--- app/components/total-item.js
+++ app/components/total-item.js
@@ -1,4 +1,14 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+
+  actions: {
+    updateChecked(ev) {
+      ev.preventDefault();
+
+      this.updateChecked({
+        checked: this.checked
+      });
+    }
+  }
 });
--- app/routes/lists/list.js
+++ app/routes/lists/list.js
@@ -2,14 +2,10 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model({ id }) {
-    return this.store.findRecord('list', id)
+    return this.store.findRecord('list', id);
   },
 
-  model() {
-    return this.store.findAll('list');
-  },
-
-  model() {
-    return this.store.findAll('item');
+  afterModel(model) {
+    return model.get('item');
   }
 });
